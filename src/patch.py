522ed09915a1156ff6640e43d56e8069d6b27087
Hongyu Cai <h.tsai@hotmail.com>
2023-11-22 09:34:52 -0500

    add dataset_id field

--- src/patch.py
+++ src/patch.py
@@ -7,19 +7,12 @@ if __name__ == "__main__":
     collection = db["data_point_collection"]
 
     # update all documents in collection, update the filed "publication_report_jailbreak_label" to bool instead of string
-    documents = collection.find({"publication_report_jailbreak_label": {"$exists": True}})
+    documents = collection.find({})
     
     for i, document in enumerate(documents):
-        publication_report_jailbreak_label = document["publication_report_jailbreak_label"]
-        if publication_report_jailbreak_label == "True":
-            update_operation = {
-                "$set": {"publication_report_jailbreak_label": True}
-            }
-        elif publication_report_jailbreak_label == "False":
-            update_operation = {
-                "$set": {"publication_report_jailbreak_label": False}
-            }
-        else:
-            assert False
+        dataset_id = document["publication_id"]
+        update_operation = {
+            "$set": {"dataset_id": dataset_id}
+        }
         collection.update_one({"_id": document["_id"]}, update_operation)
         print(i)
