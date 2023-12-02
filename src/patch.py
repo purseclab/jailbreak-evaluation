d517b3c6e09e5c2d02cdf9f52dacb76107f93550
Hongyu Cai <h.tsai@hotmail.com>
2023-12-02 12:21:55 -0500

    update

--- src/patch.py
+++ src/patch.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
     collection = db["data_point_collection"]
 
     # update all documents in collection, update the filed "publication_report_jailbreak_label" to bool instead of string
-    documents = collection.find({})
+    # find all documents which do not have the filed "publication_report_jailbreak_label"
+    documents = collection.find({"dataset_version": {"$exists": False}})
     
     for i, document in enumerate(documents):
-        dataset_id = document["publication_id"]
+        # dataset_id = document["publication_id"]
         update_operation = {
-            "$set": {"dataset_id": dataset_id}
+            "$set": {"dataset_version": 0}
         }
         collection.update_one({"_id": document["_id"]}, update_operation)
         print(i)
