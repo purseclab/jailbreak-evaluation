0b6dbe74da4a1ba1ad982dbbde3592f412693ddf
mdctleo <mdctleo@gmail.com>
2023-12-17 16:54:08 -0500

    formatting

--- src/language_models.py
+++ src/language_models.py
@@ -3,6 +3,7 @@ import os
 from typing import Dict, List
 import time
 
+
 class ChaoGPT():
     API_RETRY_SLEEP = 10
     API_ERROR_OUTPUT = "$ERROR$"
@@ -14,10 +15,10 @@ class ChaoGPT():
     def __init__(self, model_name) -> None:
         self.model_name = model_name
 
-    def generate(self, conv: List[Dict], 
-                max_n_tokens: int, 
-                temperature: float,
-                top_p: float):
+    def generate(self, conv: List[Dict],
+                 max_n_tokens: int,
+                 temperature: float,
+                 top_p: float):
         '''
         Args:
             conv: List of dictionaries, OpenAI API format
@@ -31,25 +32,25 @@ class ChaoGPT():
         for _ in range(self.API_MAX_RETRY):
             try:
                 response = openai.ChatCompletion.create(
-                            model = self.model_name,
-                            messages = conv,
-                            max_tokens = max_n_tokens,
-                            temperature = temperature,
-                            top_p = top_p,
-                            request_timeout = self.API_TIMEOUT,
-                            )
+                    model=self.model_name,
+                    messages=conv,
+                    max_tokens=max_n_tokens,
+                    temperature=temperature,
+                    top_p=top_p,
+                    request_timeout=self.API_TIMEOUT,
+                )
                 output = response["choices"][0]["message"]["content"]
                 break
             except openai.error.OpenAIError as e:
                 print(type(e), e)
                 time.sleep(self.API_RETRY_SLEEP)
-        
+
             time.sleep(self.API_QUERY_SLEEP)
-        return output 
-    
-    def batched_generate(self, 
-                        convs_list: List[List[Dict]],
-                        max_n_tokens: int, 
-                        temperature: float,
-                        top_p: float = 1.0,):
+        return output
+
+    def batched_generate(self,
+                         convs_list: List[List[Dict]],
+                         max_n_tokens: int,
+                         temperature: float,
+                         top_p: float = 1.0,):
         return [self.generate(conv, max_n_tokens, temperature, top_p) for conv in convs_list]
