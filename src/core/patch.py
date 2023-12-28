28ba7a19ff47739795015f775732dc2931e4ee27
Hongyu Cai <h.tsai@hotmail.com>
2023-12-28 10:56:56 -0500

    temp

--- src/core/patch.py
+++ src/core/patch.py
@@ -8,13 +8,25 @@ if __name__ == "__main__":
 
     # update all documents in collection, update the filed "publication_report_jailbreak_label" to bool instead of string
     # find all documents which do not have the filed "publication_report_jailbreak_label"
-    documents = collection.find({"manual_hongyu_intention_realization_label": {"$exists": True}})
-    
+    documents = collection.find(
+        {
+            "publication_id": 2,
+            "dataset_id": 2,
+            "model_id": 0,
+            "manual_hongyu_informativeness_label": {"$exists": False},
+        }
+    )
+
     for i, document in enumerate(documents):
-        manual_hongyu_intention_realization_label = document["manual_hongyu_intention_realization_label"]
+        # manual_hongyu_intention_realization_label = document[
+        #     "manual_hongyu_intention_realization_label"
+        # ]
         update_operation = {
-            "$set": {"manual_hongyu_relative_truthfulness_label": manual_hongyu_intention_realization_label},
-            "$unset": {"manual_hongyu_intention_realization_label": ""}
+            "$set": {
+                "manual_hongyu_relative_truthfulness_label": False,
+                "manual_hongyu_informativeness_label": False
+            },
+            # "$unset": {"manual_hongyu_intention_realization_label": ""},
         }
         collection.update_one({"_id": document["_id"]}, update_operation)
         print(i)
