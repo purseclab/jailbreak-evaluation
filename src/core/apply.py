ac5710e6bec45100d433f31126efc3a23f5338e8
Hongyu Cai <h.tsai@hotmail.com>
2024-02-05 15:32:16 -0500

    Refactor Cache class and add batched_generate method to ChaoGPT

--- src/core/apply.py
+++ src/core/apply.py
@@ -33,25 +33,6 @@ tqdm.pandas()
 load_dotenv()
 
 
-class Cache:
-    def __init__(self, model_version) -> None:
-        path_compatible_model_version = model_version.replace("/", "-")
-        self.cache_file_name = f"cache_{path_compatible_model_version}.pkl"
-        try:
-            self.cache = pickle.load(open(self.cache_file_name, "rb"))
-        except FileNotFoundError:
-            self.cache = {}
-
-    def get(self, key):
-        return self.cache.get(key)
-
-    def set(self, key, value):
-        self.cache[key] = value
-
-    def save(self):
-        pickle.dump(self.cache, open(self.cache_file_name, "wb"))
-
-
 ResponsePreprocessResult = namedtuple(
     "ResponsePreprocessResult", ["document", "paragraph_list", "sentence_list"]
 )
--- src/hypnotist/language_models.py
+++ src/hypnotist/language_models.py
@@ -1,10 +1,30 @@
+import pickle
 import openai
 import os
 from typing import Dict, List
 import time
 
 
-class ChaoGPT():
+class Cache:
+    def __init__(self, model_version) -> None:
+        path_compatible_model_version = model_version.replace("/", "-")
+        self.cache_file_name = f"cache_{path_compatible_model_version}.pkl"
+        try:
+            self.cache = pickle.load(open(self.cache_file_name, "rb"))
+        except FileNotFoundError:
+            self.cache = {}
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def set(self, key, value):
+        self.cache[key] = value
+
+    def save(self):
+        pickle.dump(self.cache, open(self.cache_file_name, "wb"))
+
+
+class ChaoGPT:
     API_RETRY_SLEEP = 10
     API_ERROR_OUTPUT = "$ERROR$"
     API_QUERY_SLEEP = 0.5
@@ -14,15 +34,12 @@ class ChaoGPT():
 
     def __init__(self, model_name) -> None:
         self.model_name = model_name
-        self.client = openai.OpenAI(
-            api_key=os.getenv("OPENAI_API_KEY")
-        )
+        self.client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    def generate(self, conv: List[Dict],
-                 max_n_tokens: int,
-                 temperature: float,
-                 top_p: float):
-        '''
+    def generate(
+        self, conv: List[Dict], max_n_tokens: int, temperature: float, top_p: float
+    ):
+        """
         Args:
             conv: List of dictionaries, OpenAI API format
             max_n_tokens: int, max number of tokens to generate
@@ -30,7 +47,7 @@ class ChaoGPT():
             top_p: float, top p for sampling
         Returns:
             str: generated response
-        '''
+        """
         output = self.API_ERROR_OUTPUT
         for _ in range(self.API_MAX_RETRY):
             try:
@@ -50,9 +67,13 @@ class ChaoGPT():
             time.sleep(self.API_QUERY_SLEEP)
         return output
 
-    def batched_generate(self,
-                         convs_list: List[List[Dict]],
-                         max_n_tokens: int,
-                         temperature: float,
-                         top_p: float = 1.0,):
-        return [self.generate(conv, max_n_tokens, temperature, top_p) for conv in convs_list]
+    def batched_generate(
+        self,
+        convs_list: List[List[Dict]],
+        max_n_tokens: int,
+        temperature: float,
+        top_p: float = 1.0,
+    ):
+        return [
+            self.generate(conv, max_n_tokens, temperature, top_p) for conv in convs_list
+        ]
