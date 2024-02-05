faa1adc7cacd5b4fa29c78c91c34da02fdb8dee7
Hongyu Cai <h.tsai@hotmail.com>
2024-02-05 14:26:18 -0500

    Add apply_and_count.py file with implementation of f_on_row function

---
+++ src/core/apply_and_count.py
@@ -0,0 +1,55 @@
+from collections import namedtuple
+import itertools
+from urllib import response
+
+import pandas as pd
+from utils import connect_to_mongodb
+from nltk.tokenize import sent_tokenize
+
+
+def f_on_row(row):
+    intent = row["intent"]
+    stripped_intent = intent.strip()
+
+    response = row["response"]
+
+    sentences = sent_tokenize(response)
+    for sentence in sentences:
+        if stripped_intent == sentence.strip():
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    mongodb_client = connect_to_mongodb()
+
+    db = mongodb_client["main_database"]
+    collection = db["data_point_collection"]
+
+    Run = namedtuple(
+        "Run", ["publication_id", "dataset_id", "dataset_version", "model_id"]
+    )
+
+    documents = collection.find({})
+    df = pd.DataFrame(documents)
+    assert len(df) == 700
+
+    for run in [Run(0, 0, 0, 0), Run(1, 1, 0, 0), Run(2, 2, 0, 1)]:
+        publication_id = run.publication_id
+        dataset_id = run.dataset_id
+        dataset_version = run.dataset_version
+        model_id = run.model_id
+        run_rows = df[
+            (df["publication_id"] == publication_id)
+            & (df["dataset_id"] == dataset_id)
+            & (df["dataset_version"] == dataset_version)
+            & (df["model_id"] == model_id)
+        ]
+
+        x = run_rows.apply(
+            lambda row: f_on_row(row),
+            axis=1,
+        )
+
+        
+        print(x.value_counts())
